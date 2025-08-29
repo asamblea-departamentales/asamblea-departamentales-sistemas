a0be2f578cceb422c376ce65cd3b8fdc0d7e5e57
Stven2909 <143978815+Stven2909@users.noreply.github.com>
2025-08-28 23:59:54 -0600

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,15 @@
 <?php
-
 namespace Tests\Feature;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-
 class ExampleTest extends TestCase
 {
     use RefreshDatabase;
-
     /** @test */
     public function test_the_application_returns_a_successful_response()
     {
-        // Accede directamente a la ruta de login del panel
-        $response = $this->get('/admin/login');
-
-        // Debe devolver 200 OK
+        // Usa la ruta de contacto que sí existe
+        $response = $this->get('/contact');
         $response->assertStatus(200);
     }
 }
