cc1dbf6b71a681a05a03a413a43151e0e16cde6b
Stven2909 <143978815+Stven2909@users.noreply.github.com>
2025-08-29 00:22:04 -0600

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,21 +6,11 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     use RefreshDatabase;
-    
-    protected function setUp(): void
-    {
-        parent::setUp();
-        
-        // Clear caches before test
-        $this->artisan('route:clear');
-        $this->artisan('config:clear');
-    }
 
     /** @test */
     public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/contact');
         $response->assertStatus(200);
-        $response->assertSeeText('Contact GET works');
     }
 }
