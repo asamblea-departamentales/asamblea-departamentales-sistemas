2571b7702acdaf0656b9aaa4ba62b3a764188467
Stven2909 <143978815+Stven2909@users.noreply.github.com>
2025-08-28 23:31:42 -0600

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,8 +9,7 @@ class ExampleTest extends TestCase
     /** @test */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->withoutMiddleware()->get('/');
-
+        $response = $this->withoutMiddleware()->get('/contact'); // ajustado a formulario de contacto
         $response->assertStatus(200);
     }
 }
