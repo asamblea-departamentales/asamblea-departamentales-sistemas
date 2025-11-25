400e304d7a2f8cb02487d746b7123c4549b4d63e
Steven Rivera <steven.melendez001@gmail.com>
2025-11-24 18:57:59 -0600

    Optimizacion del impersonate

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -43,7 +43,6 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->defaultLocale('es')
             ->locales(['es'])
 
             // Auth
