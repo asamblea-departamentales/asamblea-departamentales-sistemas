9484bf2502dea2b78714e209608a2cb61b8f288f
Steven Rivera <steven.melendez001@gmail.com>
2025-12-09 21:31:51 -0600

    Reanudar Cierres

--- resources/views/pdf/cierre_mensual.blade.php
+++ resources/views/pdf/cierre_mensual.blade.php
@@ -94,38 +94,57 @@
         </tr>
     </table>
 
-    {{-- Actividades por programa --}}
-    @php
-        $actividadesPorPrograma = $cierre->actividades->groupBy('programa');
-    @endphp
+   {{-- Actividades por programa --}}
+@php
+// Normalizamos nombres de programas para evitar problemas de tildes o saltos de línea
+$mapaProgramas = [
+    'Atencion Ciudadana' => 'Atención Ciudadana',
+    'Participacion Ciudadana' => 'Participación Ciudadana',
+    'Educacion Civica' => 'Educación Cívica',
+];
 
-    @foreach(['Atención Ciudadana','Participación Ciudadana','Educación Cívica'] as $programa)
-        <div class="section-title">Programa de {{ $programa }}</div>
-        <table>
-            <tr>
-                <th>#</th>
-                <th>Macroactividad</th>
-                <th>Estado</th>
-                <th>Asistentes</th>
-                <th>Hombres</th>
-                <th>Mujeres</th>
-            </tr>
-            @forelse($actividadesPorPrograma[$programa] ?? collect() as $i => $actividad)
-                <tr>
-                    <td>{{ $i+1 }}</td>
-                    <td>{{ $actividad->macroactividad }}</td>
-                    <td>{{ $actividad->estado }}</td>
-                    <td>{{ $actividad->asistencia_total }}</td>
-                    <td>{{ $actividad->asistentes_hombres }}</td>
-                    <td>{{ $actividad->asistentes_mujeres }}</td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="6">No hay actividades registradas para este programa.</td>
-                </tr>
-            @endforelse
-        </table>
-    @endforeach
+$actividadesPorPrograma = $cierre->actividades->groupBy(function($actividad) use ($mapaProgramas) {
+    // Limpia espacios y saltos de línea
+    $nombre = preg_replace('/\s+/', ' ', trim($actividad->programa));
+    // Quita tildes para comparar
+    $nombreSinTilde = str_replace(
+        ['á','é','í','ó','ú','Á','É','Í','Ó','Ú'],
+        ['a','e','i','o','u','A','E','I','O','U'],
+        $nombre
+    );
+    // Devuelve el nombre oficial si existe en el mapa
+    return $mapaProgramas[$nombreSinTilde] ?? $nombre;
+});
+@endphp
+
+@foreach(['Atención Ciudadana','Participación Ciudadana','Educación Cívica'] as $programa)
+<div class="section-title">Programa de {{ $programa }}</div>
+<table>
+    <tr>
+        <th>#</th>
+        <th>Macroactividad</th>
+        <th>Estado</th>
+        <th>Asistentes</th>
+        <th>Hombres</th>
+        <th>Mujeres</th>
+    </tr>
+    @forelse($actividadesPorPrograma[$programa] ?? collect() as $i => $actividad)
+        <tr>
+            <td>{{ $i+1 }}</td>
+            <td>{{ $actividad->macroactividad }}</td>
+            <td>{{ $actividad->estado }}</td>
+            <td>{{ $actividad->asistencia_total }}</td>
+            <td>{{ $actividad->asistentes_hombres }}</td>
+            <td>{{ $actividad->asistentes_mujeres }}</td>
+        </tr>
+    @empty
+        <tr>
+            <td colspan="6">No hay actividades registradas para este programa.</td>
+        </tr>
+    @endforelse
+</table>
+@endforeach
+
 
     {{-- Programación mensual --}}
     <div class="section-title">Actividades Programadas para el Mes</div>
