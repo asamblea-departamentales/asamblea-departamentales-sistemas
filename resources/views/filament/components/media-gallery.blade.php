eac70c7402563e220becb1a2b0cd23b73191ec6f
Steven Rivera <steven.melendez001@gmail.com>
2026-03-25 20:47:37 -0600

    add: Vista personalizada para Atestados #2

--- resources/views/filament/components/media-gallery.blade.php
+++ resources/views/filament/components/media-gallery.blade.php
@@ -1,33 +1,37 @@
 <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-    @foreach ($media as $file)
-        <div class="border rounded-xl p-2 shadow-sm bg-white">
 
-            {{-- Imagen preview --}}
+    @forelse ($media as $file)
+        <div class="border rounded-xl p-2 shadow-sm bg-white hover:shadow-md transition">
+
+            {{-- Preview --}}
             @if(str_contains($file->mime_type, 'image'))
                 <img src="{{ $file->getUrl() }}" 
                      class="rounded-lg w-full h-32 object-cover" />
             @else
-                <div class="h-32 flex items-center justify-center bg-gray-100 rounded-lg">
+                <div class="h-32 flex items-center justify-center bg-gray-100 rounded-lg text-2xl">
                     📄
                 </div>
             @endif
 
             {{-- Nombre --}}
-            <p class="text-xs mt-2 truncate">
+            <p class="text-xs mt-2 truncate font-medium">
                 {{ $file->name }}
             </p>
 
             {{-- Acciones --}}
             <div class="flex justify-between mt-2 text-xs">
-                <a href="{{ $file->getUrl() }}" target="_blank" class="text-blue-600">
+                <a href="{{ $file->getUrl() }}" target="_blank" class="text-blue-600 hover:underline">
                     Ver
                 </a>
 
-                <a href="{{ $file->getUrl() }}" download class="text-green-600">
+                <a href="{{ $file->getUrl() }}" download class="text-green-600 hover:underline">
                     Descargar
                 </a>
             </div>
 
         </div>
-    @endforeach
+    @empty
+        <p class="text-gray-500 text-sm">No hay archivos adjuntos.</p>
+    @endforelse
+
 </div>
