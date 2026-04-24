a109faf89b8796789d62d0c39e38c87f26ea7482
Steven Rivera <steven.melendez001@gmail.com>
2026-04-24 09:59:50 -0600

    fixed: Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -25,13 +25,17 @@ class Handler extends ExceptionHandler
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            if ($this->shouldReport($e)) {
-                FilamentExceptions::report($e);
-            }
-        });
-    }
+{
+    $this->reportable(function (Throwable $e) {
+        if (
+            $this->shouldReport($e) &&
+            !app()->runningInConsole() &&
+            app()->bound('request')
+        ) {
+            FilamentExceptions::report($e);
+        }
+    });
+}
 
     // Para las tostadas
     public function render($request, Throwable $exception)
