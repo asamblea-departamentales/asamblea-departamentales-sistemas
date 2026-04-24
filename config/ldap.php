3890dfbb6f31e49c030de8331e2c51bef98690f7
Steven Rivera <steven.melendez001@gmail.com>
2026-04-24 11:04:13 -0600

    fixed: LDAP config

--- config/ldap.php
+++ config/ldap.php
@@ -1,14 +1,17 @@
 <?php
-'connections' => [
-    'default' => [
-        'hosts' => [env('LDAP_HOSTS', '172.19.10.7')],
-        'port' => env('LDAP_PORT', 389),
-        'base_dn' => env('LDAP_BASE_DN', 'ou=Empleados,DC=asamblea,DC=gob,DC=sv'),
-        'username' => env('LDAP_USERNAME', 'ASAMBLEA\\apache'),
-        'password' => env('LDAP_PASSWORD', 'mapache'),
-        'use_ssl' => env('LDAP_USE_SSL', false),
-        'use_tls' => env('LDAP_USE_TLS', false),
-        'version' => 3,
-        'timeout' => 5,
+
+return [
+    'connections' => [
+        'default' => [
+            'hosts' => [env('LDAP_HOSTS', '172.19.10.7')],
+            'port' => env('LDAP_PORT', 389),
+            'base_dn' => env('LDAP_BASE_DN', 'ou=Empleados,DC=asamblea,DC=gob,DC=sv'),
+            'username' => env('LDAP_USERNAME', 'ASAMBLEA\\apache'),
+            'password' => env('LDAP_PASSWORD', 'mapache'),
+            'use_ssl' => env('LDAP_USE_SSL', false),
+            'use_tls' => env('LDAP_USE_TLS', false),
+            'version' => 3,
+            'timeout' => 5,
         ],
     ],
+];
